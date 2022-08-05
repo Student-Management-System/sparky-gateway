7aa743a9fa241bbe8515d3420c8e73091764f7b6
mspark <sparkm@uni-hildesheim.de>
2022-08-05 11:33:39 +0200

    refactor: descriptive method structure for security configuration

--- src/main/java/net/ssehub/sparkyservice/api/routing/HttpSecurityConfig.java
+++ src/main/java/net/ssehub/sparkyservice/api/routing/HttpSecurityConfig.java
@@ -3,7 +3,6 @@ package net.ssehub.sparkyservice.api.routing;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
@@ -11,33 +10,36 @@ import net.ssehub.sparkyservice.api.routing.matching.PermissionRouteMatcher;
 import net.ssehub.sparkyservice.api.routing.matching.RouteMatcherFactory;
 
 @Configuration
-@EnableWebFluxSecurity
-public class SecurityConfig {
+public class HttpSecurityConfig {
 
     @Autowired
     private ServerHttpSecurity http;
 
     @Bean
-    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http, RouteMatcherFactory factory) {
-        factory.createMatchers().collectList().block().stream().forEach(this::gatewayRoutePermConfig);
-        defaultEndpointConfig(http);
+    public SecurityWebFilterChain springSecurityFilterChain(RouteMatcherFactory factory) {
+        factory.createMatchers().collectList().block().stream().forEach(this::configureSecurityWhenNeeded);
+        defaultEndpointConfig();
         return http.build();
     }
 
-    private void gatewayRoutePermConfig(PermissionRouteMatcher routeMatcher) {
-        var authConfig = http.authorizeExchange();
+    private void configureSecurityWhenNeeded(PermissionRouteMatcher routeMatcher) {
         if (routeMatcher.needsAuthentication()) {
-            if (routeMatcher.getNeededPermissions().length > 0) {
-                authConfig.matchers(routeMatcher).hasAnyAuthority(routeMatcher.getNeededPermissions());
-            } else {
-                authConfig.matchers(routeMatcher).authenticated();
-            }
+            configureRoutePermissions(routeMatcher);
         } else {
-            authConfig.matchers(routeMatcher).permitAll();
+            http.authorizeExchange().matchers(routeMatcher).permitAll();
         }
     }
 
-    private void defaultEndpointConfig(ServerHttpSecurity http) {
+    private void configureRoutePermissions(PermissionRouteMatcher routeMatcher) {
+        var matcherConfig = http.authorizeExchange().matchers(routeMatcher);
+        if (routeMatcher.getNeededPermissions().length > 0) {
+            matcherConfig.hasAnyAuthority(routeMatcher.getNeededPermissions());
+        } else {
+            matcherConfig.authenticated();
+        }
+    }
+
+    private void defaultEndpointConfig() {
         http.authorizeExchange()
             .pathMatchers("/health").permitAll()
             .pathMatchers("/check").authenticated()
